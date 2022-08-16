7a9aa7f262e2ad4de6253b565e220e3683a32fbd
Happy <biswasnedhi09@gmail.com>
2022-08-15 22:06:22 -0500

    added new classes in the SchoolPractice project

---
+++ SchoolPractice/Course.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolPractice
+{
+    internal class Course
+    {
+        //at least three field
+        //should be a list or Dictionary
+        //use of student class
+        private string subject;
+        private string teacher;
+        private int credit;
+        private Student newStudent;
+        private Dictionary<string, int> meetingRoom;
+    }
+}
---
+++ SchoolPractice/Teacher.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolPractice
+{
+    internal class Teacher
+    {
+        //auto-implemented property
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string TeachingSubject { get; set; }
+        public int YearsTeaching { get; set; }
+    }
+}
